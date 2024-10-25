351e012ecba119a7dc073c8377916335811270ae
Dương Trí Dũng <crytv778@gmail.com>
2024-10-25 13:10:34 +0700

    update bun.lockb, package.json, app.module.ts and auth.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { NoteModule } from './note/note.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, AuthModule, NoteModule],
+  imports: [UserModule, AuthModule, NoteModule, PrismaModule],
   providers: [AppService],
 })
 export class AppModule {}
